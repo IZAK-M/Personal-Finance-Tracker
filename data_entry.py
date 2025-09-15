f6134ae988894b9db1fa518852b500ee5ac095d9
IZAK-M <izakmohamed44@gmail.com>
2025-09-15 09:56:42 +0200

    Fonction terminer

--- data_entry.py
+++ data_entry.py
@@ -1,5 +1,8 @@
 from datetime import datetime
+
 date_format = "%Y-%m-%d"
+CATEGORIES = {"R": "Revenus", "D": "Dépenses"}
+
 
 def get_date(prompt, allow_default=False):
     date_str = input(prompt)
@@ -9,15 +12,35 @@ def get_date(prompt, allow_default=False):
         valid_date = datetime.strftime(date_str, date_format)
         return valid_date.strftime(date_format)
     except ValueError:
-        print("Format de date non valide. Veuillez saisir la date au format aaaa-mm-jj.")
-        return get_date(prompt,allow_default)
-    
+        print(
+            "Format de date non valide. Veuillez saisir la date au format aaaa-mm-jj."
+        )
+        return get_date(prompt, allow_default)
+
+
 def get_montant():
-    pass
+    try:
+        montant = float(input("Entrez le montant: "))
+        if montant <= 0:
+            raise ValueError(
+                "Le montant doit être une valeur non négative et différente de zéro"
+            )
+        return montant
+    except ValueError as e:
+        print(e)
+        return get_montant()
+
 
 def get_categorie():
-    pass
+    categorie = input(
+        "Entrez la catégorie ('R' pour les revenus ou 'D' pour les dépenses): "
+    ).upper()
+    if categorie in CATEGORIES:
+        return CATEGORIES[categorie]
+
+    print("Catégorie non valide. Veuillez saisir 'R' ou 'D': ")
+    return get_categorie
+
 
 def get_description():
-    pass
-
+    return input("Entrez une description (facultatif) : ")
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import csv
 from datetime import datetime
+from data_entry import get_montant, get_description, get_categorie, get_date
+
 
 class CSV:
     CSV_FILE = "finance_data.csv"
@@ -11,19 +13,31 @@ class CSV:
         try:
             pd.read_csv(cls.CSV_FILE)
         except FileNotFoundError:
-            df= pd.DataFrame(columns= cls.COLUMNS)
-            df.to_csv(cls.CSV_FILE, index = False)
+            df = pd.DataFrame(columns=cls.COLUMNS)
+            df.to_csv(cls.CSV_FILE, index=False)
 
     @classmethod
     def add_entry(cls, date, montant, categorie, description):
         new_entry = dict(
-            date = date,
-            montant = montant,
-            categorie = categorie,
-            description = description
+            date=date, montant=montant, categorie=categorie, description=description
         )
         with open(cls.CSV_FILE, "a", newline="") as csvfile:
             writer = csv.DictWriter(csvfile, fieldnames=cls.COLUMNS)
             writer.writerow(new_entry)
         print("Donées ajoutées avec succès")
 
+
+def add():
+    CSV.initialize_csv()
+
+    date = get_date(
+        "Entrez la date de la transaction (aaaa-mm-jj) Ou tapez entré pour la date d'aujourd'hui : ",
+        allow_default=True,
+    )
+    montant = get_montant()
+    categorie = get_categorie()
+    description = get_description()
+
+    CSV.add_entry(date, montant, categorie, description)
+
+add()
